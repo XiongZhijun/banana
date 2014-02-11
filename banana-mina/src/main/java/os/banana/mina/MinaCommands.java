f8ddad85fddfa18918b630cf59288e36e61f9550
XiongZhijun <hust.xzj@gmail.com>
2014-02-11 11:09:56 +0800

    定义一些默认的命令。

---
+++ banana-mina/src/main/java/os/banana/mina/MinaCommands.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright © 2014 Xiong Zhijun, All Rights Reserved.
+ * Email : hust.xzj@gmail.com
+ */
+package os.banana.mina;
+
+import os.banana.mina.command.Command;
+import os.banana.mina.command.SimpleCommand;
+
+/**
+ * @author Xiong Zhijun
+ * @email hust.xzj@gmail.com
+ * 
+ */
+public interface MinaCommands {
+
+	String SESSION_OPEN = "__session_open__";
+	String SESSION_CLOSE = "__session_close__";
+	String SERVICE_ACTIVATED = "__service_activated__";
+	String SERVICE_INACTIVATED = "__service_inactivated__";
+	String EXCEPTION_CAUGHT = "__exception_caught__";
+	String MESSAGE_RECEIVE = "__message_receive__";
+
+	Command SESSION_OPEN_COMMAND = new SimpleCommand(SESSION_OPEN);
+	Command SESSION_CLOSE_COMMAND = new SimpleCommand(SESSION_CLOSE);
+	Command SERVICE_ACTIVATED_COMMAND = new SimpleCommand(SERVICE_ACTIVATED);
+	Command SERVICE_INACTIVATED_COMMAND = new SimpleCommand(SERVICE_INACTIVATED);
+}
