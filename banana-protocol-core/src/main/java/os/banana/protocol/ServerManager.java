5692f3f9094ecaba41189ce5e1d5741e74cc317b
XiongZhijun <hust.xzj@gmail.com>
2014-02-26 11:04:18 +0800

    新增ServerManager，用来管理Server

---
+++ banana-protocol-core/src/main/java/os/banana/protocol/ServerManager.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright © 2014 Xiong Zhijun, All Rights Reserved.
+ * Email : hust.xzj@gmail.com
+ */
+package os.banana.protocol;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author Xiong Zhijun
+ * @email hust.xzj@gmail.com
+ * 
+ */
+public class ServerManager {
+	private Map<String, Server> serverMap = new HashMap<String, Server>();
+
+	public void regist(Server server) {
+		String code = server.getCode();
+		writeLock.lock();
+		try {
+			Server existServer = getServer(code);
+			if (existServer == server) {
+				return;
+			}
+			if (existServer != null) {
+				existServer.stop();
+				unregist(code);
+			}
+			serverMap.put(code, server);
+		} finally {
+			writeLock.unlock();
+		}
+	}
+
+	public Server getServer(String code) {
+		readLock.lock();
+		try {
+			return serverMap.get(code);
+		} finally {
+			readLock.unlock();
+		}
+	}
+
+	public void unregist(String code) {
+		writeLock.lock();
+		try {
+			serverMap.remove(code);
+		} finally {
+			writeLock.unlock();
+		}
+	}
+
+	private ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+	private Lock readLock = readWriteLock.readLock();
+	private Lock writeLock = readWriteLock.writeLock();
+
+	public Collection<Server> getAllServers() {
+		List<Server> servers = new ArrayList<Server>();
+		servers.addAll(serverMap.values());
+		return servers;
+	}
+
+}
---
+++ banana-protocol-core/src/test/java/os/banana/protocol/ServerManagerTest.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright © 2014 Xiong Zhijun, All Rights Reserved.
+ * Email : hust.xzj@gmail.com
+ */
+package os.banana.protocol;
+
+import static org.junit.Assert.*;
+
+import org.easymock.EasyMockRunner;
+import org.easymock.Mock;
+import org.easymock.TestSubject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.easymock.EasyMock.*;
+
+/**
+ * @author Xiong Zhijun
+ * @email hust.xzj@gmail.com
+ * 
+ */
+@RunWith(EasyMockRunner.class)
+public class ServerManagerTest {
+	@TestSubject
+	private ServerManager manager;
+	@Mock
+	private Server server1;
+	@Mock
+	private Server server2;
+
+	@Before
+	public void setUp() throws Exception {
+		manager = new ServerManager();
+	}
+
+	@Test
+	public void testRegist() {
+		expect(server1.getCode()).andReturn("server1").times(2);
+		expect(server2.getCode()).andReturn("server1");
+		server1.stop();
+
+		replay(server1, server2);
+		assertNull(manager.getServer("server1"));
+		manager.regist(server1);
+		assertEquals(server1, manager.getServer("server1"));
+		manager.regist(server1);
+		assertEquals(server1, manager.getServer("server1"));
+		manager.regist(server2);
+		assertEquals(server2, manager.getServer("server1"));
+		verify(server1, server2);
+	}
+
+	@Test
+	public void testUnregist() {
+		expect(server1.getCode()).andReturn("server1");
+		replay(server1);
+		manager.regist(server1);
+		assertEquals(server1, manager.getServer("server1"));
+		manager.unregist("server1");
+		assertNull(manager.getServer("server1"));
+		verify(server1);
+	}
+
+	@Test
+	public void testGetAllServers() {
+		expect(server1.getCode()).andReturn("server1");
+		expect(server2.getCode()).andReturn("server2");
+		replay(server1, server2);
+		assertArrayEquals(new Object[] {}, manager.getAllServers().toArray());
+		manager.regist(server1);
+		assertArrayEquals(new Object[] { server1 }, manager.getAllServers()
+				.toArray());
+		manager.regist(server2);
+		assertArrayEquals(new Object[] { server1, server2 }, manager
+				.getAllServers().toArray());
+		verify(server1, server2);
+	}
+
+}
